9e83487bfaaf745089dae8d911c4f4d5b24d97c1
Lucas <lm.spizzirri@gmail.com>
2023-10-18 12:12:00 -0300

    Req 4 com func aux

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,18 @@
 def is_anagram(first_string, second_string):
-    first_string = first_string.replace(" ", "").lower()
-    second_string = second_string.replace(" ", "").lower()
+    if len(first_string) < 1 and len(second_string) < 1:
+        return first_string.lower(), second_string.lower(), False
+    first_string = order_string(first_string)
+    second_string = order_string(second_string)
+    if not first_string == second_string:
+        return first_string, second_string, False
+    return first_string, second_string, True
 
-    if len(first_string) != len(second_string):
-        return (sorted(first_string), sorted(second_string), False)
 
-    if sorted(first_string) == sorted(second_string):
-        return (sorted(first_string), sorted(second_string), True)
-    else:
-        return (sorted(first_string), sorted(second_string), False)
+def order_string(string):
+    aplhabet = 'abcdefghijklmnopqrstuvwxyz'
+    string = string.lower()
+    ordered_string = ''
+    for letter in aplhabet:
+        if letter in string:
+            ordered_string += letter * string.count(letter)
+    return ordered_string
